fac92b407353004ea2de2b502a3bbe2f85e18d5e
admin <blckclov3r@gmail.com>
2020-10-07 12:12:22 -0700

    commit

--- wp-content/wpo-cache/config/config-localhost.php
+++
@@ -1,4 +0,0 @@
-<?php
-if (!defined('ABSPATH')) die('No direct access allowed');
-
-$GLOBALS['wpo_cache_config'] = json_decode('{"enable_page_caching":true,"page_cache_length_value":24,"page_cache_length_unit":"hours","page_cache_length":86400,"cache_exception_urls":[""],"cache_exception_cookies":[""],"cache_exception_browser_agents":[""],"enable_sitemap_preload":false,"enable_schedule_preload":"0","preload_schedule_type":"wpo_use_cache_lifespan","enable_mobile_caching":"0","enable_user_caching":"0","site_url":"https:\/\/localhost\/blckclov3r\/","enable_cache_per_country":false,"wpo_cache_cookies":[],"wpo_cache_query_variables":[]}', true);
