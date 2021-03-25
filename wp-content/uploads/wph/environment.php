4daa1bb27022f4f9008ed7108104bacf11d2f76e
aljun.abrenica@mindweb.com.ph <blckclov3r@gmail.com>
2021-03-25 03:44:19 -0700

    not generated

--- wp-content/uploads/wph/environment.php
+++ wp-content/uploads/wph/environment.php
@@ -1,2 +1,2 @@
 <?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-$environment_variable = '{"theme":{"folder_name":"kadence","mapped_name":"aljun_template"},"allowed_paths":["C:\/xampp\/htdocs\/blckclov3r\/wp-content\/themes"],"cache_path":"C:\/xampp\/htdocs\/blckclov3r\/wp-content\/cache\/wph\/","wordpress_directory":"","site_relative_path":"\/blckclov3r\/"}'; ?>
+$environment_variable = '{"theme":{"folder_name":"kadence","mapped_name":"aljun_template"},"allowed_paths":["C:\/xampp\/htdocs\/blckclov3r\/wp-content\/themes"],"cache_path":"C:\/xampp\/htdocs\/blckclov3r\/wp-content\/cache\/wph\/","wordpress_directory":"","site_relative_path":"\/"}'; ?>
