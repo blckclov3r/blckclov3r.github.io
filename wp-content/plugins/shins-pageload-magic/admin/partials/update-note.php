0ef1998a800d0e4fba2535b3392172c7f7e85e05
aljun.abrenica@mindweb.com.ph <blckclov3r@gmail.com>
2021-03-26 06:17:14 -0700

    generated

--- wp-content/plugins/shins-pageload-magic/admin/partials/update-note.php
+++
@@ -1,6 +0,0 @@
-<div id="message" class="notice notice-info fade">
-    <p>
-        <strong>💡 Tip for defer.js:</strong> To archive better result, please <u>disable</u> lazyload and HTML-CSS-JS optimizations by other plugins.
-        View <a href="<?php echo DEFER_WP_SETTINGS; ?>">plugin settings</a> here.
-    </p>
-</div>
