2909ccea8f8939476bfd82134e14302a5ef4f5d0
blckclov3r <blckclov3r@gmail.com>
2022-06-09 08:57:31 +0800

    deploy

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import { useEffect } from "react";
 function App() {
   useEffect(() => {
     window.open("https://blckclov3r.netlify.app","_self");
-     
    }, []);
   return (
     <div className="App">
+      <h1 style={{color: "#fff"}}>Blckclov3r</h1>
+      <h2 style={{color: "#fff"}}>Blckclov3r</h2>
+      <h3 style={{color: "#fff"}}>Blckclov3r</h3>
+      <h4 style={{color: "#fff"}}>Blckclov3r</h4>
+      <h5 style={{color: "#fff"}}>Blckclov3r</h5>
+      <h6 style={{color: "#fff"}}>Blckclov3r</h6>
     </div>
   );
 }
